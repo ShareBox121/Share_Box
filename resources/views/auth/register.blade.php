6032286acd0b841d62d4b7b751797581f37dd9bb
giovannayumii <100042126+giovannayumii@users.noreply.github.com>
2023-05-18 20:38:04 -0300

    AB#42

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,27 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/register.css')}}" />
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+        crossorigin="anonymous"
+    />
+</head>
+<body>
+<div class="container">
+    <div class="row vh-100 d-flex justify-content-center align-items-center">
+        <div class="col-sm-6 col-md-4">
+            <div class="card d-flex justify-content-center align-items-center bg-white">
+                <!-- <img src="{{asset('images/logo-login.svg')}}" class="logo" /> -->
+
     <form method="POST" action="{{ route('register') }}">
-        @csrf
+        @csrf 
 
         <!-- Name -->
         <div>
@@ -49,4 +70,5 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</body>
+</html>
