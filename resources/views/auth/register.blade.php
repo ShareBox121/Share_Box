5f2b89686db0180cbe3877f4e8ac8bd0964595ce
giovannayumii <100042126+giovannayumii@users.noreply.github.com>
2023-05-25 20:38:38 -0300

    AB#42

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
 <body>
 <div class="container">
     <div class="row vh-100 d-flex justify-content-center align-items-center">
-        <div class="col-sm-6 col-md-4">
+        <div class="col-sm-6 col-md-5">
             <div class="card d-flex justify-content-center align-items-center bg-white">
                 <!-- <img src="{{asset('images/logo-login.svg')}}" class="logo" /> -->
 
@@ -25,21 +25,21 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nome:')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('E-mail:')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Senha:')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -51,7 +51,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirme a senha:')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -60,15 +60,22 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+        <br>
+
+        <p class="pe-2 ps-4">Ao clicar em <span class="fw-bold">Cadastrar-se</span>, você concorda com os nossos
+            <span class="fw-bold">Termos, Políticas 
+              de Privacidade e Políticas de Cookies.</span> Você pode cancelar quando quiser. </p>
+
+        <div>
+            <a href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ml-4">
+            <x-primary-button class="btn btn-dark mt-5" > <!-- class="btn btn-dark mt-5"  -->
                 {{ __('Register') }}
             </x-primary-button>
         </div>
     </form>
+
 </body>
 </html>
