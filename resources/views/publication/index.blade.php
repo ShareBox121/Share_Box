5033c5e5cf33d5ccb27f90ce67d4d2a27ee0d520
luis4PF <luishtardem@gmail.com>
2023-06-11 00:30:26 -0300

    AB#09
    fix - personalizando publication

--- resources/views/publication/index.blade.php
+++ resources/views/publication/index.blade.php
@@ -6,15 +6,86 @@
     <head>
         <title>File Type Filter</title>
         <style>
+            .card-wrapper {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+            }
+
             .file-container {
-                display: none;
+                flex: 0 0 300px;
+                margin: 10px;
+            }
+
+            .card {
+                border: none;
+                border-radius: 10px;
+                box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+            }
+
+            .card-title {
+                font-size: 1.5rem;
+                font-weight: bold;
+                margin-bottom: 10px;
+                color: #333;
+            }
+
+            .card-text {
+                margin-bottom: 20px;
+                color: #777;
+            }
+
+            .card-body img {
+                display: block;
+                width: 100%;
+                height: auto;
+                border-radius: 10px;
+                box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+            }
+
+            .card-body embed {
+                display: block;
+                width: 100%;
+                height: auto;
+                border-radius: 10px;
+                box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+            }
+
+            .card-body a {
+                width: 100%;
+                background-color: #333;
+                color: #fff;
+                border: none;
+                border-radius: 5px;
+                padding: 10px 15px;
+                font-weight: bold;
+                text-transform: uppercase;
+                text-decoration: none;
+                transition: background-color 0.3s ease;
+            }
+
+            .card-body a:hover {
+                background-color: #555;
+            }
+
+            .filter-container {
+                flex: 0 0 300px;
+                margin: 10px;
+            }
+
+            .filter-container select {
+                border: none;
+                padding: 10px 20px;
+                border-radius: 15px;
+                box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
             }
         </style>
     </head>
 
     <body>
-        <div>
-            <label for="fileType">Filter by File Type:</label>
+        
+        <div class="filter-container mt-2 mr-3">
+            <label for="fileType">Filtrar Por:</label>
             <select id="fileType" onchange="filterFiles()">
                 <option value="all">Todos</option>
                 <option value="image">Imagens</option>
@@ -22,24 +93,22 @@
                 <option value="video">Videos</option>
             </select>
         </div>
-
         @foreach ($events as $file)
             <?php
             $fileExtension = pathinfo($file->path, PATHINFO_EXTENSION);
             ?>
-            <div class="file-container" data-filetype="{{ $fileExtension }}" style="display: block;">
-                <div class="container p-5">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div data-aos="fade-left" data-aos-duration="2000">
-                                <h1 class="title">{{ $file->title }}</h1>
-                                <br>
-                                @if ($fileExtension === 'pdf')
-                                    <embed src="{{ $file->path }}" type="application/pdf" width="500px" height="500px">
-                                @else
-                                    <img src="{{ $file->path }}" style="width:500px">
-                                @endif
-                            </div>
+            <div class="file-container" data-filetype="{{ $fileExtension }}">
+                <div class="container mx-auto mt-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $file->title }}</h5>
+                            <p class="card-text">{{ $file->description }}</p>
+                            @if ($fileExtension === 'pdf')
+                                <embed src="{{ $file->path }}" type="application/pdf" width="100%" height="250px">
+                            @else
+                                <img src="{{ $file->path }}" style="width:100%">
+                            @endif
+                            <a href="{{ $file->path }}" class="btn btn-dark mt-3" download>Baixar</a>
                         </div>
                     </div>
                 </div>
@@ -56,9 +125,9 @@
 
                     if (selectedType === '' || selectedType === 'all' ||
                         (selectedType === 'image' && (fileType === 'jpg' || fileType === 'jpeg' || fileType ===
-                        'png')) ||
-                         (selectedType === 'document' && (fileType === 'pdf' || fileType === 'txt')) ||
-                         (selectedType === 'video' && (fileType === 'mp4'))) {
+                            'png')) ||
+                        (selectedType === 'document' && (fileType === 'pdf' || fileType === 'txt')) ||
+                        (selectedType === 'video' && (fileType === 'mp4'))) {
                         container.style.display = 'block';
                     } else {
                         container.style.display = 'none';
